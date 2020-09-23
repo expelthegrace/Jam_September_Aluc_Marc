ebd744fab6d8dfa3d9a99979cb60b3c7e5042717
Alec <alec.lafita@gmail.com>
2020-09-24 00:25:26 +0200

    Added basic player movement and obstacle prefab

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D mRigidBody;
+    [SerializeField] private float mSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        mRigidBody.velocity = new Vector3(horizontal * mSpeed, vertical * mSpeed, 0.0f); ;
+    }
+}
