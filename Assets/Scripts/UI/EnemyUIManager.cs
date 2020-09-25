d678260bfaaf0f984b6361b13c5d277104122ec7
Marc <marc.palomar.soler@gmail.com>
2020-09-25 17:27:00 +0200

    UI added: enemy state countdown bar and state name

---
+++ Assets/Scripts/UI/EnemyUIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EnemyUIManager : MonoBehaviour
+{
+    public TMP_Text stateTitleText;
+    public Image stateContdownImageBar;
+
+    public GameObject mEnemy;
+    private StateManager mEnemyStateManager;
+    private EnemyState mCurrentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mEnemyStateManager = mEnemy.GetComponent<StateManager>();       
+       
+        EnemyState.mOnEnemyStateChanged.AddListener(StartCountdownnImage);
+    }
+
+    private void StartCountdownnImage()
+    {
+        StopCoroutine(UpdateCountdownImage());
+        StartCoroutine(UpdateCountdownImage());
+       
+    }
+
+    private IEnumerator UpdateCountdownImage()
+    {
+        mCurrentState = mEnemyStateManager.CurrentState;
+
+        stateTitleText.text = mCurrentState.nameState;
+
+        float StateDuration = mCurrentState.mDurationTime;
+        stateContdownImageBar.fillAmount = 1f;
+
+        float startTime = Time.time;
+        
+
+        while (stateContdownImageBar.fillAmount > 0.01f)
+        {
+            stateContdownImageBar.fillAmount = 1 - (Time.time - startTime) / StateDuration;
+            yield return null;
+        }
+    }
+
+}
+
