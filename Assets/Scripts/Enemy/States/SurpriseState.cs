9e11046a2ee6ccbf8f7aa5c7575a1fbedac41a75
Alec <alec.lafita@gmail.com>
2020-09-27 00:04:29 +0200

    Added surprise enemy state

---
+++ Assets/Scripts/Enemy/States/SurpriseState.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurpriseState : BasicState
+{
+    public GameObject mPlayer;
+    public GameObject mExtraEnemy;
+    public Vector3[] mSpawnPoints;
+
+    private float mLastTimeShootedExtraEnemy;
+    [SerializeField] private float mTimeBetweenShootingExtraEnemy = 1f;
+    [SerializeField] private float mTimeBetweenBulletExtraEnemy = 0.2f;
+    [SerializeField] private int mBulletsPerShootingExtraEnemy = 2;
+    [SerializeField] private int mNumberOfCanonsExtraEnemy = 20;
+
+    public void Start()
+    {
+        mExtraEnemy = GameObject.Find("SurpriseEnemy");
+        mPlayer = GameObject.Find("Player");
+        GameObject SpawnPoints = GameObject.Find("SpawnPoints");
+        Transform[] spawnPointsTransforms = SpawnPoints.GetComponentsInChildren<Transform>();
+        mSpawnPoints = new Vector3[spawnPointsTransforms.Length];
+        for (int i = 0; i < mSpawnPoints.Length; ++i)
+        {
+            mSpawnPoints[i] = spawnPointsTransforms[i].position;
+        }
+        mExtraEnemy.SetActive(false);
+    }
+    public override void HandleStart()
+    {
+        mDurationTime = 3f;
+        nameState = "Surprise!";
+        mStateID = 3;
+        mBulletType = BulletSpawner.eBulletType.Normal;
+
+        mExtraEnemy.SetActive(true);
+        float minDistance = float.PositiveInfinity;
+        Vector3 spawnPosition = Vector3.zero;
+        float playerMaxSize = Mathf.Max(mPlayer.GetComponent<SpriteRenderer>().bounds.size.x, mPlayer.GetComponent<SpriteRenderer>().bounds.size.y);
+        foreach (Vector3 position in mSpawnPoints)
+        {
+            float currentPositionDistance = Vector3.Distance(mPlayer.transform.position, position);
+            if (currentPositionDistance < minDistance && currentPositionDistance > playerMaxSize)
+            {
+                minDistance = currentPositionDistance;
+                spawnPosition = position;
+            }
+        }
+        mExtraEnemy.transform.position = spawnPosition;
+
+        mLastTimeShootedExtraEnemy = Time.time;
+
+        base.BasicStart();
+    }
+
+    public override EnemyState StateUpdate()
+    {
+        mTimeStateActive = Time.time - mTimeStateActivated;
+
+        if (mTimeStateActive > mDurationTime)
+        {
+            mExtraEnemy.SetActive(false);
+            StopCoroutine(ShootBurst());
+            return GetRandomState();
+        }
+
+        return null;
+    }
+
+    public override void StateFixedUpdate()
+    {
+        base.Move();
+        base.ShootingManager();
+        
+        if (Time.time - mLastTimeShootedExtraEnemy > mTimeBetweenShootingExtraEnemy)
+        {
+            mLastTimeShootedExtraEnemy = Time.time;
+            StartCoroutine(ShootBurst());
+            CameraAnimation.Shake(0.03f, mBulletsPerShootingExtraEnemy * mTimeBetweenBulletExtraEnemy);
+        }
+    }
+
+    private IEnumerator ShootBurst()
+    {
+        float shootedTime = Time.time;
+        int bulletsShooted = 0;
+        while (bulletsShooted < mBulletsPerShootingExtraEnemy)
+        {
+            if (Time.time - shootedTime > mTimeBetweenBulletExtraEnemy)
+            {
+                shootedTime = Time.time;
+                FanShoot();
+                ++bulletsShooted;
+            }
+            yield return null;
+        }
+    }
+
+    private void FanShoot()
+    {
+        float radius = Mathf.Max(mExtraEnemy.gameObject.GetComponent<SpriteRenderer>().bounds.size.x, mExtraEnemy.gameObject.GetComponent<SpriteRenderer>().bounds.size.y);
+        float angleIncrement = (2.0f * Mathf.PI) / mNumberOfCanonsExtraEnemy;
+        for (int canon = 0; canon < mNumberOfCanonsExtraEnemy; ++canon)
+        {
+            Vector3 offset = radius * new Vector3(Mathf.Cos(angleIncrement * canon), Mathf.Sin(angleIncrement * canon), 0.0f);
+            mSpawner.SpawnBullet(mBulletType, mOwner.GetSpeedIncrement(), mExtraEnemy.gameObject.transform.position + offset, offset);
+            mShootAudio.Play();
+        }
+    }
+}
--- Assets/Scripts/Player/ShieldPart.cs
+++ Assets/Scripts/Player/ShieldPart.cs
@@ -7,6 +7,7 @@ public class ShieldPart : MonoBehaviour
     public AudioSource mCristalAudio;
 
     private const string BulletTag = "Bullet";
+    private const string EnemyTag = "Enemy";
     private const string ObstacleTag = "Obstacle";
     // Start is called before the first frame update
     void Start()
@@ -29,8 +30,7 @@ public class ShieldPart : MonoBehaviour
             Destroy(aCollision.gameObject);
             Destroy(this.gameObject);
         }
-        
-        if (aCollision.gameObject.CompareTag(ObstacleTag))
+        else if (aCollision.gameObject.CompareTag(ObstacleTag) || aCollision.gameObject.CompareTag(EnemyTag))
         {
             Destroy(this.gameObject);
         }
